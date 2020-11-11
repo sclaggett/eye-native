8efea06bcf5a7a735205ff62c30232faa3c6abe1
Shane Claggett <shaneclaggett@hushmail.com>
2020-11-11 14:54:07 -0800

    Removed unused includes

--- src/NativeWrapper.cpp
+++ src/NativeWrapper.cpp
@@ -2,12 +2,6 @@
 #include "FfmpegProcess.h"
 #include "FrameThread.h"
 #include <stdio.h>
-extern "C"
-{
-  #include <libavcodec/avcodec.h>
-  #include <libavformat/avformat.h>
-  #include <libswscale/swscale.h>
-}
 
 using namespace std;
 
